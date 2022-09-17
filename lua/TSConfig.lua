a6c0f58813126b034e77240f9f058ad9e19d1ee6
Carter Moore <cartermoore893@gmail.com>
2022-09-17 15:44:56 -0700

    Add Comment.nvim

--- lua/TSConfig.lua
+++ lua/TSConfig.lua
@@ -68,3 +68,7 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+--------------------------------------------------------------------------------
+-- Comment.nvim
+--------------------------------------------------------------------------------
+require('Comment').setup()
